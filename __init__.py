65235d2cb892972ed3a94bd6e4439ff0afbc8553
Majkel <C00231687@itcarlow.ie>
2020-11-05 23:11:04 +0100

    Updated titles

--- __init__.py
+++ __init__.py
@@ -7,15 +7,15 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("index.html", title="Home")
 	
 @app.route("/personal")
 def personal():
-	return render_template("personal.html")  
+	return render_template("personal.html", title="Personal")
 	
 @app.route("/cv")
 def cv():
-	return render_template("cv.html")
+	return render_template("cv.html", title="CV") 
 
 @app.route("/contact", methods=['GET', 'POST'])
 def contact():
@@ -24,25 +24,25 @@ def contact():
 		message = request.form.get('message')
 		file = open("comments.txt", "a+")
 		file.write(email + " " + message + "\n")
-		return render_template("sent.html")
+		return render_template("sent.html", title="Thank You")
 	else:
-		return render_template("contact.html")
+		return render_template("contact.html", title="Contact")
 	
 @app.route("/computing")
 def computing():
 	subpage = request.args.get('subpage')
 
 	pages = {
-		"1" : render_template("interest1.html"),
-		"2" : render_template("interest2.html"),
-		"3" : render_template("interest3.html")
+		"1" : render_template("interest1.html", title="Games"),
+		"2" : render_template("interest2.html", title="Music"),
+		"3" : render_template("interest3.html", title="Programming")
 	}
 
-	return pages.get(subpage, render_template("computing.html"))
+	return pages.get(subpage, render_template("computing.html", title="Computing"))
 
 @app.route("/interest")
 def interest():
-	return render_template("interest.html")
+	return render_template("interest.html", title="Interests")
 
 
 if __name__ == "__main__":
